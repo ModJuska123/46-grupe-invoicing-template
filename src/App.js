dbc0fbe7f5480a175a392b089f02a01a78e4de2c
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-11-17 17:18:31 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-//pagrindinis failas darbui
+import "./App.css";
+import Header from "./Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
 
 function App() {
-  const name = 'Modestas'
-  return (
-  <>
-    <header>Header</header>
-    <main>Hello {name}</main>
-    <footer>footer</footer>
-  </>
+  const handleNameChange = () => {
+    const name = ['Bob', 'Kevin', 'Dave'];
+    const int = Math.floor(Math.random() *3);
+    return name[int];
+  }
+
+   return (
+    <div className="App">
+      <Header />
+      <Content />
+      <Footer />
+     
+    </div>
   )
 }
 
---
+++ src/components/Content.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Content() {
+    const handleNameChange = () => {
+        const name = ['Bob', 'Kevin', 'Dave'];
+        const int = Math.floor(Math.random() *3);
+        return name[int];
+      }
+    
+       return (
+        <main>
+            <p>
+                Hello {handleNameChange()}
+            </p>
+        </main>
+        
+  )
+}
+
+export default Content
